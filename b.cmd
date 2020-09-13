2f95a2536cd89ac22fe855f2ab87ee5b8bd8ed62
gyorokpeter <gyorokpeter@freemail.hu>
2020-09-13 17:14:57 +0100

    externalize dependencies to config file

--- b.cmd
+++ b.cmd
@@ -1,1 +1,7 @@
-g++ -Wall -shared qbinparse.cpp -I../../c++/qutils -L../../c++/qutils -lq -o qbinparse.dll
+cls
+@call config.cmd
+@if not exist libq.a (
+	echo create libq.a as per https://code.kx.com/q/interfaces/using-c-functions/#windows-mingw-64
+	exit 1
+)
+g++ -Wall -shared qbinparse.cpp -I%KX_KDB_PATH%/c/c -L. -lq -o qbinparse.dll
