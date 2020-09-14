1960274babff57120fd3ee98a6e547cb3508b1c9
Péter Györök <gyorokpeter1@gmail.com>
2020-09-14 09:36:02 +0100

    exit /b

--- b.cmd
+++ b.cmd
@@ -1,7 +1,7 @@
 cls
 @call config.cmd
 @if not exist libq.a (
-	echo create libq.a as per https://code.kx.com/q/interfaces/using-c-functions/#windows-mingw-64
-	exit 1
+    echo create libq.a as per https://code.kx.com/q/interfaces/using-c-functions/#windows-mingw-64
+    exit /b 1
 )
 g++ -Wall -shared qbinparse.cpp -I%KX_KDB_PATH%/c/c -L. -lq -o qbinparse.dll
