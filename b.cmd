b209158aebb939b8500d5d718d5d48d8437ba949
gyorokpeter <gyorokpeter@freemail.hu>
2020-12-19 07:36:45 +0000

    add compiler flags

--- b.cmd
+++ b.cmd
@@ -4,4 +4,4 @@ cls
     echo create libq.a as per https://code.kx.com/q/interfaces/using-c-functions/#windows-mingw-64
     exit /b 1
 )
-g++ -Wall -shared qbinparse.cpp -I%KX_KDB_PATH%/c/c -L. -lq -o qbinparse.dll
+g++ -Wall -shared qbinparse.cpp -I%KX_KDB_PATH%/c/c -L. -lq -o qbinparse.dll -static-libgcc -static-libstdc++
